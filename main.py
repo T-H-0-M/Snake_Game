3266c5cc1d44ca73bb03f43cb0c6953e71a74282
Thomas <thomasbandy23@gmail.com>
2022-12-21 22:24:23 +1100

    Implemented OO

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
+import time
 
 # screen configuration
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.tracer(0)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-# generating the beginning of the snake
-snake = []
-for x in range(3):
-    snake.append(Turtle(shape="square"))
-    snake[x].color("white")
-    snake[x].setpos(-(x*20), 0)
+
+snake = Snake()
+screen.update()
+
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
+
+
+gameover = False
+while not gameover:
+    snake.move()
+    time.sleep(0.1)
+    screen.update()
+
 
 screen.exitonclick()
